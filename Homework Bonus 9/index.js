4cd24434e69797d391274126f66a8dc4cc0e970f
daotuanbk <daotuanbk@gmail.com>
2018-04-04 13:26:08 +0700

    Updated Homework 9

---
+++ Homework Bonus 9/index.js
@@ -0,0 +1,65 @@
+$(document).ready(() => {
+    const formElement = $(".article-search-form");
+    let timeOut = null;
+    formElement.keyup(event => {
+        event.preventDefault();
+        clearTimeout(timeOut);
+        timeOut = setTimeout(() => {;
+            listenToFormSubmitEvent();
+        }, 1000);
+    });
+})
+
+const listenToFormSubmitEvent = async () => {
+    let searchContent = $(".article-search-form__input").val();
+    console.log(searchContent);
+    let articleList = ``;
+    await reset();
+    await addLoader();
+    let data = await $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php',
+        method: 'POST',
+        data: {
+            action: "query",
+            list: "search",
+            format: "json",
+            srprop: "snippet",
+            origin: "*",
+            srsearch: encodeURI(searchContent)
+        }
+    })
+    await $('.loader').remove();
+    const findSuccessed = function () {
+        if (data.query.search.length == 0) {
+            articleList += `<h3 class = "result no-result">No result, please search another keywords<h3>`;
+        } else {
+            for (let i = 0; i < data.query.search.length; i++) {
+                articleList += `<a href="https://en.wikipedia.org/?curid=${data.query.search[i].pageid}" target="_blank" class="article-view result" > 
+                    <h3 className="article-view__title">${data.query.search[i].title}</h3>     
+                    <p className="article-view__snippet">${data.query.search[i].snippet}</p> </a>`;
+            }
+        }
+        if (articleList != ``) {
+            $(".article-list").append(articleList);
+        }
+    }
+    findSuccessed();
+
+
+
+    /**
+     * TODO:
+     *  - Lấy từ khoá search của người dùng (done)
+     *  - Lấy data từ server wikipedia tương ứng với từ khoá search  (done)
+     *  - Từ data trả về, tạo một array DOM hiển thị các bài viết của wikipedia (done)
+     *  - Đưa các DOM trong array trên vào trong <div class="article-list"></div> (done)
+     */
+}
+
+const reset = () => {
+    $('.result').remove();
+}
+
+const addLoader = () => {
+    $('.app').append(`<div class="loader"></div>`);
+}
